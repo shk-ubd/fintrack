dc2860569a4b938b5cc739885e5a2066fa7daf94
Sheikh Ubaid Ullah <sheikh.ubaidullah10@gmail.com>
2026-01-14 01:55:50 +0500

    initial - project structure - folder structure

---
+++ src/app/components/custom-button/custom-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  imports: [],
+  templateUrl: './custom-button.html',
+  styleUrl: './custom-button.css',
+})
+export class CustomButton {
+
+}
